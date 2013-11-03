25cc9bb5717e52ee6e2f86165e6c96048e25788b
Cesar Robles <chechare_94@hotmail.com>
2013-11-03 12:59:04 -0600

    Re-actualizar diseño

--- reActualizar.php
+++ reActualizar.php
@@ -1,18 +1,35 @@
 <?php
+include "superior.php";
+?>
+
+<div class="large-9 columns">
+
+
+<fieldset><legend>Reporte de Incidencias </legend>
+<?php
 
 include 'sql.php';
 if(isset($_POST['aconte']) && $_POST['aconte'] == 1){
 	echo '
-    <form action="reActualizar.php" method="post">Estado donde acontecio algun hecho que pudo poner tu integridad fisica en riesgo:<br>
-    <select name="estado">
-    <option></option>';
+		
+		<form action="reActualizar.php" method="post"><p>¿En qu&eacute; estado o lugar te encontrabas cuando sucedi&oacute; el acontecimiento que puso en riesgo tu integridad f&iacute;sica?</p>   
+		<div class="large-6 columns">
+	<select name="estado">
+    <option></option>
+	</div>
+	'
+	;
     
     $Estados = mysql_query('SELECT DISTINCT id FROM areas');
 	while($rowEstados = mysql_fetch_row($Estados)){
 		echo'<option value="'.$rowEstados[0].'">'.$rowEstados[0].'</option>';
 	}
-    echo '</select><br><textarea name="acont" style="width:150px; height:100px;" placeholder="Que ocurrio? espesifica lo mas posible"></textarea><br>
+    echo '
+	
+	</select><br><textarea name="acont" style="width:100%; height:100%;" placeholder="¿Qu&eacute; fue lo qu&eacute; sucesi&oacute;? Por favor explica detalladamente para as&iacute; poder ayudar a otros compa&ntilde;eros migrantes."></textarea><br>
+	
     <input type="submit" value="Enviar">
+	
     
     <input type="hidden" name="nombre" value="'.$_POST['nombre'].'" >
     <input type="hidden" name="referencia" value="'.$_POST['referencia'].'" />
@@ -21,7 +38,9 @@ if(isset($_POST['aconte']) && $_POST['aconte'] == 1){
 	<input type="hidden" name="ciudad" value="'.$_POST['ciudad'].'" />
     <input type="hidden" name="aconte" value="'.$_POST['aconte'].'" />
 	<input type="hidden" name="salud" value="'.$_POST['salud'].'" />
-    </form>';
+    </form>
+	
+	';
 	
 }else{
 	$error = false;
@@ -63,3 +82,11 @@ if(isset($_POST['aconte']) && $_POST['aconte'] == 1){
 }
 
 ?>
+</fieldset>
+
+</div>
+
+<?php
+include "inferior.php";
+?>
+      
