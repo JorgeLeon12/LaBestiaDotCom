a60d5896281961d3774b52b0478407be216bde6d
Jesus290 <jesusgarcia290@hotmail.com>
2013-11-03 09:13:48 -0600

    index

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+include"superior.php";
+?>
+
+      <div class="large-9 columns">
+        <div class="large-12 columns" style="border-radius: 5px">
+          <fieldset><legend class="legend1">Innded</legend>
+            <div class="large-12 columns">
+              <div class="large-4 columns"><a href="http://www.indeed.com.mx/" target="_blank"><img src="img/indeed.png"></a></div>
+              <div class="large-8 columns">
+                <label for="informacion">Uno de los insumos para la realización del encuentro “Los impactos de la crisis en los migrantes: visión desde México y Estados Unidos” fue una serie de diagnósticos sobre la migración en México, tanto del país en su conjunto, como en ocho estados de la República Mmexicana que consideramos representativos, ya sea por ser estados tradicionalmente expulsores de migrantes, así como ejemplos de nuevos flujos migratorios. Dichos entidades son Jalisco, Michoacán, Guanajuato, Veracruz, Puebla, Zacatecas, Distrito Federal, Oaxaca y Morelos.
+                </label>
+              </div>
+            </div>  
+          </fieldset>
+        </div>
+      
+        <div class="large-12 columns" id="info">
+          <fieldset><legend class="legend1">USA Jobs</legend>
+            <div class="large-12 columns">
+              <div class="large-4 columns"><a href="https://www.usajobs.gov/" target="_blank"><img src="img/usaJobs.png"></a></div>
+              <div class="large-8 columns">
+                <label for="informacion">Uno de los insumos para la realización del encuentro “Los impactos de la crisis en los migrantes: visión desde México y Estados Unidos” fue una serie de diagnósticos sobre la migración en México, tanto del país en su conjunto, como en ocho estados de la República Mmexicana que consideramos representativos, ya sea por ser estados tradicionalmente expulsores de migrantes, así como ejemplos de nuevos flujos migratorios. Dichos entidades son Jalisco, Michoacán, Guanajuato, Veracruz, Puebla, Zacatecas, Distrito Federal, Oaxaca y Morelos.
+                </label>
+              </div>
+            </div>  
+          </fieldset>
+        </div>
+      
+        <div class="large-12 columns" id="info">
+          <fieldset><legend class="legend1">Job Search USA</legend>
+            <div class="large-12 columns">
+              <div class="large-4 columns"><a href="http://www.jobsearchusa.org/" target="_blank"><img src="img/jobSearch.png"></a></div>
+              <div class="large-8 columns">
+                <label for="informacion">Uno de los insumos para la realización del encuentro “Los impactos de la crisis en los migrantes: visión desde México y Estados Unidos” fue una serie de diagnósticos sobre la migración en México, tanto del país en su conjunto, como en ocho estados de la República Mmexicana que consideramos representativos, ya sea por ser estados tradicionalmente expulsores de migrantes, así como ejemplos de nuevos flujos migratorios. Dichos entidades son Jalisco, Michoacán, Guanajuato, Veracruz, Puebla, Zacatecas, Distrito Federal, Oaxaca y Morelos.
+                </label>
+              </div>
+            </div>  
+          </fieldset>
+        </div>
+
+        <div class="large-12 columns" id="info">
+          <fieldset><legend class="legend1">Portal del Empleo</legend>
+            <div class="large-12 columns">
+              <div class="large-4 columns"><a href="http://www.empleo.gob.mx/" target="_blank"><img src="img/portalEmpleo.png"></a></div>
+              <div class="large-8 columns">
+                <label for="informacion">Uno de los insumos para la realización del encuentro “Los impactos de la crisis en los migrantes: visión desde México y Estados Unidos” fue una serie de diagnósticos sobre la migración en México, tanto del país en su conjunto, como en ocho estados de la República Mmexicana que consideramos representativos, ya sea por ser estados tradicionalmente expulsores de migrantes, así como ejemplos de nuevos flujos migratorios. Dichos entidades son Jalisco, Michoacán, Guanajuato, Veracruz, Puebla, Zacatecas, Distrito Federal, Oaxaca y Morelos.
+                </label>
+              </div>
+            </div>  
+          </fieldset>
+        </div>
+      </div>
+<?php
+include"inferior.php";
+?>
